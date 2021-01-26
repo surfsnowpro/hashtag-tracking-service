8b6b53d87a2076eaa4c4dd35a39799060918c092
James Hartman <jimmy@spiredigital.com>
2021-01-26 11:48:35 -0700

    add redis config

---
+++ src/main/java/com/codespring/trackinghashtagservice/TrackingHashtagServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codespring.trackinghashtagservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingHashtagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingHashtagServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codespring/trackinghashtagservice/TrackingHashtagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codespring.trackinghashtagservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingHashtagServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
